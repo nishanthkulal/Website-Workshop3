3e6713d83827fd9e1798054f675495b3e447ddfd
nishanthkulal <nishanthkulal65462@gmail.com>
2023-05-26 11:17:43 +0530

    new updates

---
+++ simple-blog/src/view/footer.php
@@ -0,0 +1,17 @@
+
+<footer>
+    <div class="container">
+        <div>
+            <nav class="hide-on-signup">
+                <a href="#">Footer link</a>
+                <a href="#">Footer link</a>
+                <a href="#">Footer link</a>
+            </nav>
+        </div>
+        <div class="is-hidden-mobile">
+            <p>
+                © 2022 mysite.com
+            </p>
+        </div>
+    </div>
+</footer>
---
+++ simple-blog/src/view/header.php
@@ -0,0 +1,23 @@
+
+<header>
+    <nav id="main-nav" class="navbar">
+        <div class="container">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="/index.php">
+                    <img src="./images/logo.png" alt="UniCourt">
+                </a>
+                <div class="navbar-burger burger" id="nav-toggle">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+
+            <div id="navbar-example" class="navbar-menu">
+                <div class="navbar-end">
+                    <a class="navbar-item is-active" href="index.php">Blog</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
