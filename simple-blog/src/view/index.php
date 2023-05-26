74c431185494ad7152686d660f468e072cc21bbf
nishanthkulal <nishanthkulal65462@gmail.com>
2023-05-26 11:17:20 +0530

    updates

--- simple-blog/src/view/index.php
+++ simple-blog/src/view/index.php
@@ -7,50 +7,28 @@
         <link rel="stylesheet" href="./css/style.css" />
 	</head>
     <body>
-        <header>
-            <nav id="main-nav" class="navbar">
-                <div class="container">
-                    <div class="navbar-brand">
-                        <a class="navbar-item" href="/index.php">
-                            <img src="./images/logo.png" alt="UniCourt">
-                        </a>
-                        <div class="navbar-burger burger" id="nav-toggle">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                        </div>
-                    </div>
+        
+            
+    <?php 
+        include './header.php';
+        // Paste blogs code below this.
+    ?>
 
-                    <div id="navbar-example" class="navbar-menu">
-                        <div class="navbar-end">
-                            <a class="navbar-item is-active" href="index.php">Blog</a>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        </header>
+        
         <div class="container">
             <div class="wrapper">
                 <!-- Paste section code below -->
 
+                
+
             </div>
         </div>
 
-        <footer>
-            <div class="container">
-                <div>
-                    <nav class="hide-on-signup">
-                        <a href="#">Footer link</a>
-                        <a href="#">Footer link</a>
-                        <a href="#">Footer link</a>
-                    </nav>
-                </div>
-                <div class="is-hidden-mobile">
-                    <p>
-                        © 2022 mysite.com
-                    </p>
-                </div>
-            </div>
-        </footer>
+            
+    <?php 
+        include './footer.php';
+    ?>
+
+        
     </body>
 </html>
